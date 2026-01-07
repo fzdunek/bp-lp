c10d0886d5e379d79317a82a9fb7f0c67c1c948b
Filip <9900496+fzdunek@users.noreply.github.com>
2026-01-07 15:30:47 +0100

    :bug: add bp me card

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -32,7 +32,7 @@ final class RankingService
                 return [
                     'place' => $index + 1,
                     'name' => $item->name,
-                    'bpme_card_number' => $item->bpme_card_number,
+                    'bpme_card_number' => $item->user->bpme_card_number,
                     'score' => $item->max_result,
                     'datetime' => Carbon::parse($item->latest_timestamp)->format('H:i:s d.m.Y'),
                 ];
@@ -70,7 +70,7 @@ final class RankingService
                 return [
                     'place' => $index + 1,
                     'name' => $item->name,
-                    'bpme_card_number' => $item->bpme_card_number,
+                    'bpme_card_number' => $item->user->bpme_card_number,
                     'score' => $item->max_result,
                     'datetime' => Carbon::parse($item->latest_timestamp)->format('H:i:s d.m.Y'),
                 ];
